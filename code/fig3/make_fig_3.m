dae98165073bd017842a65e3fa734648e2b612fb
gitlab@srinivas.gs <code@srinivas.gs>
2019-03-12 15:57:12 -0400

    improvements to fig 3

--- code/fig3/make_fig_3.m
+++ code/fig3/make_fig_3.m
@@ -1,5 +1,4 @@
-% helper script to make figures
-% for a talk
+
 
 close all
 addpath('../')
@@ -15,16 +14,16 @@ load([model_hash '_0.voronoi'],'-mat')
 x0 = sum(v.data.g0(2:3));
 y0 = sum(v.data.g0([1 4 5 6 8]));
 
-figure('outerposition',[300 300 1200 700],'PaperUnits','points','PaperSize',[1200 701]); hold on
-ax.noreg = subplot(1,2,1); hold on
-ax.flow = subplot(1,2,2); hold on
+figure('outerposition',[300 300 1400 700],'PaperUnits','points','PaperSize',[1400 701]); hold on
+ax.noreg = subplot(1,3,1); hold on
+ax.flow = subplot(1,3,2); hold on
+ax.diff = subplot(1,3,3); hold on
 
 
-v.findBoundaries(ax.noreg)
+v.plotBoundaries(ax.noreg)
 p = ax.noreg.Children;
 uistack(p(2),'top')
 c = lines(10);
-scatter(x0,y0,48,c(5,:),'o','filled')
 
 
 for i = 1:length(p)
@@ -45,6 +44,7 @@ x = singleCompartment.makeNeuron();
 singleCompartment.disableControllers(x)
 x.t_end = 5e3;
 
+v0 = v;
 
 % plot equi-calcium line
 
@@ -57,8 +57,6 @@ plot(ax.noreg,X,Y,'r','LineWidth',2)
 plot(ax.flow,X,Y,'r','LineWidth',2)
 
 set(ax.noreg,'XScale','log','YScale','log','XLim',v.x_range,'YLim',v.y_range)
-
-
 
 
 % plot the integral control solutioon
@@ -92,6 +90,35 @@ figlib.pretty('plw',1,'lw',1,'fs',18)
 
 plot(ax.noreg,[v.x_range(1) v.x_range(2)], [v.y_range(1) v.y_range(2)],'k--');
 plot(ax.flow,[v.x_range(1) v.x_range(2)], [v.y_range(1) v.y_range(2)],'k--');
+plot(ax.diff,[v.x_range(1) v.x_range(2)], [v.y_range(1) v.y_range(2)],'k--');
+
+
+% now subtract one map from the other
+R0 = v0.findBoundaries;
+R = v.findBoundaries;
+
+v_diff = voronoiSegment;
+v_diff.x_range = v.x_range;
+v_diff.y_range = v.y_range;
+
+v_diff.n_classes = 4;
+
+
+RD = NaN*R;
+RD(R0 ~= 3 & R == 3) = 1; % sensitive to perturb, comp. restores function
+RD(R0 ~= 3 & R ~= 3) = 2; % sensitive to perturb, comp. pathalogical
+RD(R0 == 3 & R ~= 3) = 3; % robust to perturb, comp. pathalogical
+RD(R0 == 3 & R == 3) = 4; % robust to perturb, comp. restores function
+
+v_diff.traceBoundaries(RD);
+
+c = prism;
+c(1:2,:) = [];
+c(3,:) = [0 0 0];
+c(4,:) = [0 0 1];
+v_diff.plotBoundaries(ax.diff,c);
+
+
 
 
 % show some trajectories
@@ -159,20 +186,46 @@ plot(ax.flow,xx,yy,'k+')
 xlabel(ax.noreg,'$\Sigma \bar{g}_{Ca} (\mu S/mm^2)$','interpreter','latex')
 ylabel(ax.noreg,'$\Sigma \bar{g}_{others} (\mu S/mm^2)$','interpreter','latex')
 
+
+xlabel(ax.diff,'$\Sigma \bar{g}_{Ca} (\mu S/mm^2)$','interpreter','latex')
+ylabel(ax.diff,'$\Sigma \bar{g}_{others} (\mu S/mm^2)$','interpreter','latex')
+
 xlabel(ax.flow,'$\Sigma \bar{g}_{Ca} (\mu S/mm^2)$','interpreter','latex')
 ylabel(ax.flow,'$\Sigma \bar{g}_{others} (\mu S/mm^2)$','interpreter','latex')
 
 axis(ax.noreg,'square')
 axis(ax.flow,'square')
+axis(ax.diff,'square')
 
+
+ax.diff.Position(2) = .25;
+ax.flow.Position(2) = .25;
+ax.noreg.Position(2) = .25;
+
+ax.diff.XTick = ax.flow.XTick;
 
 set(ax.flow,'XLim',[v.x_range(1),v.x_range(2)],'YLim',[v.y_range(1),v.y_range(2)])
 set(ax.noreg,'XLim',[v.x_range(1),v.x_range(2)],'YLim',[v.y_range(1),v.y_range(2)])
+set(ax.diff,'XLim',[v.x_range(1),v.x_range(2)],'YLim',[v.y_range(1),v.y_range(2)])
 
 
 title(ax.noreg,'Without regulation')
 title(ax.flow,'With regulation')
 
-figlib.label('y_offset',-.04,'font_size',34,'x_offset',-.03)
+figlib.label('y_offset',-.13,'font_size',34,'x_offset',-.02)
+
+% add a new axis for the text labels
+ax.txt = axes;
+ax.txt.Position = [ax.diff.Position(1)-.05 0 .3 .3];
+clear th
+th(1) = text(ax.txt,0,.2,'Sensitive to perturbation, compensation restores function','Color',[.8 .8 0]);
+th(2) = text(ax.txt,0,.4,'Sensitive to perturbation, compensation pathalogical','Color',c(2,:));
+th(3) = text(ax.txt,0,.6,'Robust to perturbation, compensation pathalogical','Color',c(3,:));
+th(4) = text(ax.txt,0,.8,'Robust to perturbation, compensation restores function','Color',c(4,:));
 
 
+for i = 1:4
+	th(i).FontWeight = 'bold';
+	th(i).FontSize = 17;
+end
+axis(ax.txt,'off')
